1fec87541d3e34880e5608c98c491888da9de139
JacobisEpic <108195485+JacobisEpic@users.noreply.github.com>
2024-04-07 19:59:58 -0400

    Updated with P2P

---
+++ client.py
@@ -0,0 +1,46 @@
+import socket
+import threading
+import json
+
+nickname = input("Choose your nickname: ")
+chatroom = input("Choose your chatroom (1, 2, or 3): ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    client.connect(('127.0.0.1', 55550))
+    print("Connected to the chat server successfully.")
+except:
+    print("Failed to connect to the chat server.")
+    exit()
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            message_data=message.split('|')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            elif message == 'CHATROOM':
+                client.send(chatroom.encode('ascii'))
+            else:
+                if len(message_data) == 3:
+                    userName = message_data[1]
+                    userMessage= message_data[2]
+                    total = userName + ': ' + userMessage
+                    print(total)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = input('')
+        message_data = f"{chatroom}|{nickname}|{message}"
+        client.send(message_data.encode('ascii'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
---
+++ server.py
@@ -0,0 +1,65 @@
+import socket
+import threading
+import json
+
+host = '127.0.0.1'
+port = 55550
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((host, port))
+server.listen()
+
+print(f"Server started on {host}:{port}")
+
+clients = []
+nicknames = []
+chatrooms = []
+
+def broadcast(message, chatroom):
+    for i, client in enumerate(clients):
+        if chatrooms[i] == chatroom:
+            client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            message_data = message.split('|')
+            chatroom = message_data[0]
+            broadcast_message = '|'.join(message_data).encode('ascii')
+            broadcast(broadcast_message, chatroom)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            chatroom = chatrooms[index]
+            left_message = f"{chatroom}|{nickname}|left!".encode('ascii')
+            broadcast(left_message, chatroom)
+            nicknames.remove(nickname)
+            chatrooms.remove(chatroom)
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        client.send('CHATROOM'.encode('ascii'))
+        chatroom = client.recv(1024).decode('ascii')
+        
+        nicknames.append(nickname)
+        clients.append(client)
+        chatrooms.append(chatroom)
+
+        print(f"Nickname: {nickname}, Chatroom: {chatroom}")
+        broadcast(json.dumps({"nickname": nickname, "chatroom": chatroom, "message": "joined!"}).encode('ascii'), chatroom)
+        client.send('Connected to server!'.encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+receive()
